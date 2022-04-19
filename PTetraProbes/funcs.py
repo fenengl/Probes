55139f9899a2ef3c400515cc026d0a14d591af5b
sayanadhikari <sayanadhikari207@gmail.com>
2022-04-19 13:23:10 +0200

    PTetra runs

---
+++ PTetraProbes/funcs.py
@@ -0,0 +1,88 @@
+"""
+Useful functions for PTetra workshop
+Author: Sigvald Marholm, 2022
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import re
+import langmuir as l
+
+def read_hst(folder_name):
+    """ Read PTetra history file in folder """
+
+    file_name = os.path.join(folder_name, 'pictetra.hst')
+
+    column_names = [
+        'timestep',
+        'time',
+        'netot',
+        'nitot',
+        'Te_eff',
+        'pot1',
+        'sc_phi_0',
+        'sc_q_0',
+        'sc_i_0',
+        'sc_phi_1',
+        'sc_q_1',
+        'sc_i_1',
+    ]
+
+    df = pd.read_csv(file_name, sep='\s+', skiprows=2, names=column_names, index_col=0)
+
+    # Add new column sc_i_tot for total collected spacecraft current
+    df = df.assign(sc_i_tot=lambda a: a.sc_i_0 + a.sc_i_1)
+
+    return df
+
+def parse_parameters(folder_name):
+    """
+    Parses geometry and plasma parameters from folder name and the pictetra.dat
+    file inside it, and returns a geometry and Species object from Langmuir.
+
+    Example:
+
+        geometry, electron, voltages = parse_parameters('Sphere_0.5R_3V_3V')
+        print(geometry)
+        print(electron.debye)
+
+    """
+
+    dat_file = os.path.join(folder_name, 'pictetra.dat')
+
+    with open(dat_file) as file:
+        lines = file.read()
+
+    # Read parameters from pictetra.dat
+    density = float(re.search(r'\s+ne\s*=\s*(\S*)', lines).groups()[0])
+    temperature = float(re.search(r'\s+te\s*=\s*(\S*)', lines).groups()[0])
+    voltages = re.search(r'\$begin sc_fixedPot\s*(\S*)\s*(\S*)', lines)
+    voltages = list(map(float, voltages.groups()))
+
+    # Create Langmuir species
+    electron = l.Electron(n=density, eV=temperature)
+    # electron = l.Electron(n=density, eV=0.051704)
+    # Read geometry from folder name
+    radius = float(re.search(r'_([^_]*)R', folder_name).groups()[0])
+    if 'cylinder' in folder_name.lower():
+        length = float(re.search(r'_([^_]*)L', folder_name).groups()[0])
+        geometry = l.Cylinder(r=radius*electron.debye, l=length*electron.debye)
+    else: # sphere
+        geometry = l.Sphere(r=radius*electron.debye)
+
+    return geometry, electron, voltages
+
+def average(df, column, relaxation_time):
+    """
+    Perform exponential moving average on a column in a Pandas dataframe. The
+    relaxation time is in seconds.
+
+    Example:
+
+        df = read_hst('Sphere_0.5R_3V_3V')
+        sc_i_tot_av = average(df, 'sc_i_tot', 1e-6)
+    """
+    delta_t = df.time[1]-df.time[0]
+    alpha = 1-np.exp(-delta_t/relaxation_time)
+    return df[column].ewm(alpha=alpha).mean()
---
+++ PTetraProbes/plot.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Plotting script for PTetra
+Author: Sigvald Marholm, 2022
+"""
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import langmuir as l
+from funcs import *
+from argparse import ArgumentParser
+from os.path import join as pjoin
+
+parser = ArgumentParser(description="Plotting tool for PTetra workshop")
+parser.add_argument('folder', help='Folder to PTetra simulation')
+parser.add_argument('column', default='sc_i_tot', nargs='?', help='Which column of pictetra.hst to plot')
+parser.add_argument('-r', metavar='tau', type=float, default=1e-7, help='Relaxation time')
+parser.add_argument('--OML', action='store_true', help='Compare current with OML theory')
+parser.add_argument('--FL', action='store_true', help='Compare current with finite-length theory')
+args = parser.parse_args()
+
+df = read_hst(args.folder)
+geometry, electron, voltages = parse_parameters(args.folder)
+
+av = average(df, args.column, args.r)
+last_value = av.iat[-1]
+
+if args.OML:
+    comparison_value = l.OML_current(geometry, electron, V=voltages[0])
+
+if args.FL:
+    comparison_value = l.finite_radius_current(geometry, electron, V=voltages[0])
+    # comparison_value = l.finite_length_current(geometry, electron, V=voltages[0])
+
+geometry_str = "cylinder" if isinstance(geometry, l.Cylinder) else "sphere"
+
+print("Geometry:           {}".format(geometry_str))
+print("Radius:             {:.2f} debye lengths".format(geometry.r/electron.debye))
+if geometry_str=="cylinder":
+        print("Length:             {:.1f} debye lengths".format(geometry.l/electron.debye))
+print("Temperature:        {:.2f} K ({:.3g} eV)".format(electron.T, electron.eV))
+print("Density:            {:.3g} /m^3".format(electron.n))
+print("Debye length:       {:.3g} mm".format(electron.debye*1e3))
+print()
+
+print("Last plotted value: {:.3g}".format(last_value))
+
+if args.OML or args.FL:
+    print("Comparsion current: {:.3g}".format(comparison_value*1e6))
+    print("Error:              {:.2f}%".format(100*(last_value-comparison_value)/comparison_value, "%"))
+    plt.axhline(comparison_value, linestyle='--', color='k', label='Comparison')
+
+plt.plot(df.time*1e6, df[args.column], '#ccc', zorder=-100, label='Raw')
+plt.plot(df.time*1e6, av, label=r'Averaged ($\tau={:g}\,\mathrm{{\mu s}}$)'.format(args.r*1e6))
+plt.xlabel('Time [us]')
+plt.ylabel('Quantity: {}'.format(args.column))
+# plt.ylabel('Current [A]')
+plt.legend()
+plt.savefig(pjoin(args.folder,args.column+'_.png'),dpi=300)
+plt.show()
