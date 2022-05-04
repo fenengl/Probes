135e2c4a603b5ef7b43075cf7703f495aef01fe2
Florine Enengl <florinee@eduroam-193-157-188-100.wlan.uio.no>
2022-05-04 13:44:35 +0200

    re-ordered code

---
+++ checks/finite_radius.py
@@ -0,0 +1,25 @@
+from langmuir import *
+import numpy as np
+import matplotlib.pyplot as plt
+from finite_radius_extrapolated import *
+
+# plasma = [ # Approximately F-region plasma
+#     Electron(n=1e11, T=1000),
+#     # Oxygen(n=1e11, T=1000)
+#     ]
+electron =      Electron(n=1e11, T=1000)
+
+geometry = Sphere(r=3.33*electron.debye)
+
+V = np.linspace(-1, 2, 100)
+eta = np.linspace(0, 25, 100)
+
+I_OML = OML_current(geometry, electron, eta=eta, normalization='th')
+I_FR = finite_radius_current(geometry, electron, eta=eta, normalization='th')
+
+plt.plot(eta, I_OML, label='OML')
+plt.plot(eta, I_FR, label='FR')
+plt.xlabel('eta')
+plt.ylabel('I/I_th')
+plt.legend()
+plt.show()
